3633f37207a9b788ec79f3b622743217b79edcb4
mofeisike <2214663365@qq.com>
2019-08-06 09:50:13 +0800

    spring clound test

---
+++ configclient/src/main/java/com/mofesike/configclient/controller/HelloHandler.java
@@ -0,0 +1,23 @@
+package com.mofesike.configclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * description: HelloHandler <br>
+ * date: 2019/8/6 9:45 <br>
+ */
+@RestController
+@RequestMapping("/hello")
+public class HelloHandler {
+
+    @Value("${server.port}")
+    private String port;
+
+    @GetMapping("/index")
+    public String index() {
+        return this.port;
+    }
+}
